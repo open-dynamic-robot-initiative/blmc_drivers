6d72b21d20b9766f641cc7ebeaad862b738d7abe
Julian Viereck <jviereck@tuebingen.mpg.de>
2017-07-10 14:05:59 +0200

    Fix the blmc_can_demo_optoforce_dumper_plot.py code

--- demo/blmc_can_demo_optoforce_dumper_plot.py
+++ demo/blmc_can_demo_optoforce_dumper_plot.py
@@ -1,3 +1,5 @@
+# Copyright 2017, Max Planck Society. All rights reserved.
+
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -9,14 +11,21 @@ IDX_TIME_MS = 0
 IDX_COUNTER = 1
 IDX_SAMPLE_COUNTER = 2
 
-plt.plot(my_data[:, IDX_COUNTER], my_data[:, -3:])
-plt.show(block=False)
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 12), dpi=80)
 
-plt.plot(my_data[:, IDX_SAMPLE_COUNTER], my_data[:, -3:])
-plt.show(block=False)
+[ax.set_title(title) for (ax, title) in zip(axes, [
+	'F_{x, y, z} [N] vs Program Counter',
+	'F_{x, y, z} [N] vs Package Counter',
+	'Ratio "Package Counter" vs "Program Counter"',
+])]
+axes[0].plot(my_data[:, IDX_COUNTER], my_data[:, -3:])
 
+axes[1].plot(my_data[:, IDX_SAMPLE_COUNTER], my_data[:, -3:])
 
-plt.plot(my_data[:, IDX_COUNTER], 
-         (my_data[:, IDX_SAMPLE_COUNTER] - my_data[0, IDX_SAMPLE_COUNTER])/(my_data[:, IDX_COUNTER] + 1))
-plt.ylim([0, 1])
+axes[2].plot(my_data[:, IDX_COUNTER], 
+	(my_data[:, IDX_SAMPLE_COUNTER] - my_data[0, IDX_SAMPLE_COUNTER])/
+    (my_data[:, IDX_COUNTER] + 1))
+axes[2].set_ylim([0, 1])
+
+plt.tight_layout()
 plt.show()
