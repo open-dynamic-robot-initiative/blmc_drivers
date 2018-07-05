d39ba6c69d04b8eaafac0d2b38401fcf7ccdc891
manuel <manuel.wuthrich@gmail.com>
2018-07-05 18:06:06 +0200

    put time logging stuff into separate file and tested

--- new_api/new_api_demo.cpp
+++ new_api/new_api_demo.cpp
@@ -40,17 +40,14 @@
 #include <functional>
 
 
-
 #include <iostream>
 #include <stdlib.h>
 
-
-
-#include <chrono>
 #include <eigen3/Eigen/Core>
 
 
 #include <array>
+#include <time_logger.hpp>
 
 
 
@@ -59,8 +56,7 @@ long unsigned count_xenomai_mode_switches = 0;
 
 
 // TODO: make sure that mode switcheds are actually detected
-static void
-action_upon_switch(int sig __attribute__((unused)))
+void action_upon_switch(int sig __attribute__((unused)))
 {
   void *bt[32];
   int nentries;
@@ -70,16 +66,6 @@ action_upon_switch(int sig __attribute__((unused)))
 
   rt_printf("MOOOOODE SWIIIIITCH\n");
   exit(-1);
-
-  /* Dump a backtrace of the frame which caused the switch to
-     secondary mode: */
-  /*
-  nentries = backtrace(bt,sizeof(bt) / sizeof(bt[0]));
-  backtrace_symbols_fd(bt,nentries,fileno(stdout));
-
-  getchar();
-  */
-
 }
 
 // GLOBALS
@@ -101,136 +87,6 @@ void cleanup_and_exit(int sig)
     }
     exit(0);
 }
-
-
-template<unsigned LENGTH> class TimeLogger
-{
-private:
-    double min_interval_;
-    double max_interval_;
-    double total_interval_;
-
-    Eigen::Matrix<double, LENGTH, 1> intervals_;
-
-    long unsigned count_;
-    long unsigned print_period_;
-
-    double interval_start_;
-
-    std::string name_;
-
-public:
-    TimeLogger(std::string name = "time logger", long unsigned print_period = 0)
-    {
-        min_interval_ = std::numeric_limits<double>::max();
-        max_interval_ = std::numeric_limits<double>::min();
-        total_interval_;
-        intervals_.setZero();
-
-        count_ = 0;
-        print_period_ = print_period;
-
-        interval_start_ = std::numeric_limits<double>::quiet_NaN();
-
-        name_ = name;
-    }
-
-    void start_interval(double interval_start)
-    {
-        interval_start_ = interval_start;
-    }
-    void start_interval()
-    {
-        start_interval(current_time());
-    }
-
-    void end_interval(double interval_end)
-    {
-        if(!std::isnan(interval_start_))
-        {
-            add_interval(interval_end - interval_start_);
-        }
-
-        if(print_period_ && count_ % print_period_ == 0)
-        {
-            print_status();
-        }
-    }
-    void end_interval()
-    {
-        end_interval(current_time());
-    }
-
-    void end_and_start_interval()
-    {
-        double time = current_time();
-        end_interval(time);
-        start_interval(time);
-    }
-
-    // getters ----------------------------------------------------------
-    double current_time() const
-    {
-        return double(rt_timer_read()) / 1000000.;
-    }
-
-    double max_interval() const
-    {
-        if(count_ < LENGTH)
-        {
-            return std::numeric_limits<double>::quiet_NaN();
-        }
-
-        return max_interval_;
-    }
-
-    double min_interval() const
-    {
-        if(count_ < LENGTH)
-        {
-            return std::numeric_limits<double>::quiet_NaN();
-        }
-
-        return min_interval_;
-    }
-
-    double avg_interval() const
-    {
-        if(count_ < LENGTH)
-        {
-            return std::numeric_limits<double>::quiet_NaN();
-        }
-
-        return total_interval_ / double(LENGTH);
-    }
-
-    void print_status() const
-    {
-        rt_printf("%s --------------------------------\n", name_.c_str());
-        rt_printf("count: %d\n min_interval: %f\n max_interval: %f\n avg_interval: %f\n",
-                  count_, min_interval(), max_interval(), avg_interval());
-        rt_printf("--------------------------------------------\n");
-
-    }
-
-    long unsigned count() const
-    {
-        return count_;
-    }
-
-private:
-    void add_interval(double interval)
-    {
-        min_interval_ = interval < min_interval_ ? interval : min_interval_;
-        max_interval_ = interval > max_interval_ ? interval : max_interval_;
-
-        long unsigned index = count_ % LENGTH;
-        total_interval_ += interval - intervals_[index];
-        intervals_[index] = interval;
-
-        count_++;
-    }
-};
 
 
 // new class created to replace CAN_Frame_t,
---
+++ new_api/time_logger.hpp
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <native/timer.h>
+#include <rtdk.h>
+
+#include <limits>
+#include <eigen3/Eigen/Core>
+
+
+
+
+template<unsigned LENGTH> class TimeLogger
+{
+private:
+    double min_interval_;
+    double max_interval_;
+    double total_interval_;
+
+    Eigen::Matrix<double, LENGTH, 1> intervals_;
+
+    long unsigned count_;
+    long unsigned print_period_;
+
+    double interval_start_;
+
+    std::string name_;
+
+public:
+    TimeLogger(std::string name = "time logger", long unsigned print_period = 0)
+    {
+        min_interval_ = std::numeric_limits<double>::max();
+        max_interval_ = std::numeric_limits<double>::min();
+        total_interval_;
+        intervals_.setZero();
+
+        count_ = 0;
+        print_period_ = print_period;
+
+        interval_start_ = std::numeric_limits<double>::quiet_NaN();
+
+        name_ = name;
+    }
+
+    void start_interval(double interval_start)
+    {
+        interval_start_ = interval_start;
+    }
+    void start_interval()
+    {
+        start_interval(current_time());
+    }
+
+    void end_interval(double interval_end)
+    {
+        if(!std::isnan(interval_start_))
+        {
+            add_interval(interval_end - interval_start_);
+        }
+
+        if(print_period_ && count_ % print_period_ == 0)
+        {
+            print_status();
+        }
+    }
+    void end_interval()
+    {
+        end_interval(current_time());
+    }
+
+    void end_and_start_interval()
+    {
+        double time = current_time();
+        end_interval(time);
+        start_interval(time);
+    }
+
+    // getters ----------------------------------------------------------
+    double current_time() const
+    {
+        return double(rt_timer_read()) / 1000000.;
+    }
+
+    double max_interval() const
+    {
+        if(count_ < LENGTH)
+        {
+            return std::numeric_limits<double>::quiet_NaN();
+        }
+
+        return max_interval_;
+    }
+
+    double min_interval() const
+    {
+        if(count_ < LENGTH)
+        {
+            return std::numeric_limits<double>::quiet_NaN();
+        }
+
+        return min_interval_;
+    }
+
+    double avg_interval() const
+    {
+        if(count_ < LENGTH)
+        {
+            return std::numeric_limits<double>::quiet_NaN();
+        }
+
+        return total_interval_ / double(LENGTH);
+    }
+
+    void print_status() const
+    {
+        rt_printf("%s --------------------------------\n", name_.c_str());
+        rt_printf("count: %d\n min_interval: %f\n max_interval: %f\n avg_interval: %f\n",
+                  count_, min_interval(), max_interval(), avg_interval());
+        rt_printf("--------------------------------------------\n");
+
+    }
+
+    long unsigned count() const
+    {
+        return count_;
+    }
+
+private:
+    void add_interval(double interval)
+    {
+        min_interval_ = interval < min_interval_ ? interval : min_interval_;
+        max_interval_ = interval > max_interval_ ? interval : max_interval_;
+
+        long unsigned index = count_ % LENGTH;
+        total_interval_ += interval - intervals_[index];
+        intervals_[index] = interval;
+
+        count_++;
+    }
+};
