784ba1177ba1d2e351e27010f3eee00e1d4e27b5
manuel <manuel.wuthrich@gmail.com>
2018-08-20 11:29:43 +0200

    cleaned analog sensor

--- new_api/devices/analogsensor.hpp
+++ new_api/devices/analogsensor.hpp
@@ -23,33 +23,25 @@ public:
 
 class Analogsensor: public AnalogsensorInterface
 {
-    std::map<std::string, std::string> sensor_to_board_name_;
-    std::string name_;
-
-    enum Measurement {analog, measurement_count};
-
-    std::vector<std::shared_ptr<const ScalarTimeseries>> measurement_;
-
-    std::shared_ptr<CanMotorboard> board_;
+    std::shared_ptr<MotorboardInterface> board_;
+    size_t sensor_id_;
 
 public:
-    Analogsensor(std::shared_ptr<CanMotorboard> board, bool sensor_id):
-        board_(board)
-    {
-        measurement_.resize(measurement_count);
-        if(sensor_id == 0)
-        {
-            measurement_[analog] = board_->measurement(CanMotorboard::analog_0);
-        }
-        else
-        {
-            measurement_[analog] = board_->measurement(CanMotorboard::analog_1);
-        }
-    }
+    Analogsensor(std::shared_ptr<MotorboardInterface> board, bool sensor_id):
+        board_(board),
+        sensor_id_(sensor_id)
+    { }
 
     virtual std::shared_ptr<const ScalarTimeseries> measurement() const
     {
-        return measurement_[analog];
+        if(sensor_id_ == 0)
+        {
+            return board_->measurement(MotorboardInterface::analog_0);
+        }
+        else
+        {
+            return board_->measurement(MotorboardInterface::analog_1);
+        }
     }
 
 };
