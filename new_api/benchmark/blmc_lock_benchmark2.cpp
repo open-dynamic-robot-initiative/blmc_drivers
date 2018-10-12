946a2c3020776a9b9105f54d29bf08cac9e78153
manuel <manuel.wuthrich@gmail.com>
2018-10-12 16:41:22 +0200

    creating simplified example

--- new_api/benchmark/blmc_lock_benchmark2.cpp
+++ new_api/benchmark/blmc_lock_benchmark2.cpp
@@ -11,40 +11,39 @@
 
 #include <utils/timer.hpp>
 
-typedef ThreadsafeTimeseries<double> ScalarTimeseries;
-
-
-
-/**
- * Computes the time as target = a - b;
- */
-void timespec_sub(struct timespec *target, struct timespec *a, struct timespec *b)
-{
-    if (b->tv_nsec > a->tv_nsec) {
-        target->tv_nsec = (a->tv_nsec - b->tv_nsec) + 1000000000;
-        target->tv_sec = a->tv_sec - b->tv_sec - 1;
-    } else {
-        target->tv_nsec = a->tv_nsec - b->tv_nsec;
-        target->tv_sec = a->tv_sec - b->tv_sec;
-    }
-}
 
 ThreadsafeTimeseries<double> series(1000);
 
-static void
-#ifndef __XENO__
-*
-#endif
-thread_body_locking(void* index)
+
+typedef long int Index;
+typedef long double Timestamp;
+
+std::shared_ptr<std::vector<double>> history_elements_;
+std::shared_ptr<std::vector<Timestamp>> history_timestamps_;
+
+Index oldest_timeindex_;
+Index newest_timeindex_;
+Index tagged_timeindex_;
+
+std::shared_ptr<osi::ConditionVariable> condition_;
+std::shared_ptr<osi::Mutex> mutex_;
+
+
+
+
+
+
+
+static THREAD_FUNCTION_RETURN_TYPE thread_body_locking(void* index)
 {
     int print_counter = 1000000;
 
-//    cpu_set_t cpuset;
-//    CPU_ZERO(&cpuset);
-//    CPU_SET(*((int*)(index)) + 1, &cpuset);
-//    int rc = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+    //    cpu_set_t cpuset;
+    //    CPU_ZERO(&cpuset);
+    //    CPU_SET(*((int*)(index)) + 1, &cpuset);
+    //    int rc = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
     // if (rc != 0) {
-      // printf("Error calling pthread_setaffinity_np: %d\n", rc);
+    // printf("Error calling pthread_setaffinity_np: %d\n", rc);
     // }
 
     osi::realtime_printf("Hello world from thread id=%d, cpu=%d\n", syscall(SYS_gettid), sched_getcpu());
@@ -53,7 +52,7 @@ thread_body_locking(void* index)
     {
         Timer<1> timer;
 
-        double d = 0.;
+        double element = 0.;
         size_t cpu_index = sched_getcpu();
         bool cpu_switch = false;
         for(size_t i = 0; i < print_counter; i++)
@@ -65,11 +64,53 @@ thread_body_locking(void* index)
 
             timer.end_and_start_interval();
 
-//            osi::sleep_ms(0.000000001);
+            //            osi::sleep_ms(0.000000001);
 
-            series.append(d);
-            d = series.newest_element();
-            d += 1.;
+
+            // append ----------------------------------------------------------
+            {
+                std::unique_lock<osi::Mutex> lock(*mutex_);
+                newest_timeindex_++;
+                if(newest_timeindex_ - oldest_timeindex_ + 1
+                        > history_elements_->size())
+                {
+                    oldest_timeindex_++;
+                }
+                Index history_index = newest_timeindex_ % history_elements_->size();
+                (*history_elements_)[history_index] = element;
+                (*history_timestamps_)[history_index] = Timer<>::current_time_ms();
+            }
+            condition_->notify_all();
+            // -----------------------------------------------------------------
+            Index timeindex;
+            {
+                std::unique_lock<osi::Mutex> lock(*mutex_);
+                while(newest_timeindex_ < oldest_timeindex_)
+                {
+                    condition_->wait(lock);
+                }
+
+                timeindex = newest_timeindex_;
+            }
+
+            {
+                std::unique_lock<osi::Mutex> lock(*mutex_);
+
+                while(newest_timeindex_ < timeindex ||
+                      newest_timeindex_ < oldest_timeindex_)
+                {
+                    condition_->wait(lock);
+                }
+
+                if(timeindex < oldest_timeindex_)
+                {
+                    timeindex = oldest_timeindex_;
+                }
+                element = (*history_elements_)[timeindex % history_elements_->size()];
+            }
+
+
+            element += 1.;
         }
         osi::realtime_printf("%d store and reads from thread %d, cpu=%d, cpu_switch=%d\n",
                              print_counter,
@@ -80,47 +121,24 @@ thread_body_locking(void* index)
     }
 }
 
-static void
-#ifndef __XENO__
-*
-#endif
-thread_body_math(void* instance_pointer)
-{
-  struct timespec now;
-  struct timespec prev;
-  struct timespec elapsed;
-
-  double d = 0.;
-  long long i = 0;
-  int print_counter = 1;
-
-  Eigen::MatrixXd m = Eigen::MatrixXd::Random(1024, 1024);
-
-  osi::realtime_printf("Start benchmarking.\n");
-
-  clock_gettime(CLOCK_REALTIME, &now);
-
-  while (true) {
-    m = m * m;
-    i ++;
-
-    if (i % print_counter == 0) {
-      clock_gettime(CLOCK_REALTIME, &now);
-      timespec_sub(&elapsed, &now, &prev);
-
-      float elapsed_ms = (float)(elapsed.tv_sec)*1000 + (elapsed.tv_nsec/1e6);
-      osi::realtime_printf(
-        "Duration %0.6f ms for %d matrix multiplies. m(42, 42)=%0.3f\n",
-        elapsed_ms, print_counter, m(512, 512));
-
-      prev = now;
-    }
-  }
-}
 
 
 int main(int argc, char **argv)
 {
+    int max_length = 1000;
+
+    oldest_timeindex_ = 0;
+    newest_timeindex_ = oldest_timeindex_ - 1;
+
+
+    history_elements_ = std::make_shared<std::vector<double>>(max_length);
+    history_timestamps_ = std::make_shared<std::vector<Timestamp>>(max_length);
+
+    condition_ = std::make_shared<osi::ConditionVariable>();
+    mutex_ = std::make_shared<osi::Mutex>();
+
+
+
     mlockall(MCL_CURRENT | MCL_FUTURE);
 
     osi::initialize_realtime_printing();
@@ -131,13 +149,11 @@ int main(int argc, char **argv)
     for(size_t i = 0; i < indices.size(); i++)
         indices[i] = i;
 
-    if (argc == 1) {
-      for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         osi::start_thread(&thread_body_locking, &indices[i]);
-      }
-    } else if (argc == 2) {
-      osi::start_thread(&thread_body_math, NULL);
     }
+
 
     while(true)
     {
