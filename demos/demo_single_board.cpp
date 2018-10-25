ece32f719d4ae4d9d551d413a5e6a97b2a821540
Steve Heim <heim@is.mpg.de>
2018-10-25 14:03:59 +0200

    added some code for testing quadruped

---
+++ demos/demo_single_board.cpp
@@ -0,0 +1,117 @@
+#include <blmc_drivers/devices/motor.hpp>
+#include <blmc_drivers/devices/analog_sensor.hpp>
+#include <blmc_drivers/utils/timer.hpp>
+
+class Controller
+{
+private:
+    std::shared_ptr<blmc_drivers::Motor> motor_;
+    std::shared_ptr<blmc_drivers::AnalogSensor> analog_sensor_;
+
+public:
+    Controller(std::shared_ptr<blmc_drivers::Motor> motor,
+               std::shared_ptr<blmc_drivers::AnalogSensor> analog_sensor):
+        motor_(motor), analog_sensor_(analog_sensor) { }
+
+    void start_loop()
+    {
+        osi::start_thread(&Controller::loop, this);
+    }
+
+private:
+    /**
+     * @brief this function is just a wrapper around the actual loop function,
+     * such that it can be spawned as a posix thread.
+     */
+    static THREAD_FUNCTION_RETURN_TYPE loop(void* instance_pointer)
+    {
+        ((Controller*)(instance_pointer))->loop();
+    }
+
+
+    /**
+     * @brief this is a simple control loop which runs at a kilohertz.
+     *
+     * it reads the measurement from the analog sensor, in this case the
+     * slider. then it scales it and sends it as the current target to
+     * the motor.
+     */
+    void loop()
+    {
+        Timer<10> time_logger("controller");
+        while(true)
+        {
+            double analog_measurement =
+                    analog_sensor_->get_measurement()->newest_element();
+            double current_target = 4 * (analog_measurement - 0.5);
+
+            motor_->set_current_target(current_target);
+            motor_->send_if_input_changed();
+
+            // print -----------------------------------------------------------
+            Timer<>::sleep_ms(1);
+            time_logger.end_and_start_interval();
+            if ((time_logger.count() % 1000) == 0)
+            {
+                rt_printf("sending current: %f\n", current_target);
+                // time_logger.print_status();
+            }
+        }
+    }
+};
+
+
+
+
+int main(int argc, char **argv)
+{  
+    osi::initialize_realtime_printing();
+
+    // create bus and boards -------------------------------------------------
+#ifdef __XENO__
+    auto can_bus1 = std::make_shared<blmc_drivers::CanBus>("rtcan0");
+    // auto can_bus2 = std::make_shared<blmc_drivers::CanBus>("rtcan1");
+#else
+    auto can_bus1 = std::make_shared<blmc_drivers::CanBus>("can0");
+    auto can_bus2 = std::make_shared<blmc_drivers::CanBus>("can1");
+    auto can_bus3 = std::make_shared<blmc_drivers::CanBus>("can2");
+#endif
+    auto board1 = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus1);
+    auto board2 = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus2);
+    auto board3 = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus3);
+    // create motors and sensors ---------------------------------------------
+    auto motor_1 = std::make_shared<blmc_drivers::Motor>(board1, 0);
+    auto motor_2 = std::make_shared<blmc_drivers::Motor>(board1, 1);
+    auto motor_3 = std::make_shared<blmc_drivers::Motor>(board2, 0);
+    auto motor_4 = std::make_shared<blmc_drivers::Motor>(board2, 1);
+
+    auto motor_5 = std::make_shared<blmc_drivers::Motor>(board3, 0);
+
+    rt_printf("motors are set up \n");
+
+    // auto analog_sensor_1 = std::make_shared<blmc_drivers::AnalogSensor>(board1, 0);
+    // auto analog_sensor_2 = std::make_shared<blmc_drivers::AnalogSensor>(board1, 1);
+    auto analog_sensor_3 = std::make_shared<blmc_drivers::AnalogSensor>(board2, 0);
+
+    rt_printf("sensors are set up \n");
+
+    Controller controller1(motor_1, analog_sensor_3);
+    Controller controller2(motor_2, analog_sensor_3);
+    Controller controller5(motor_5, analog_sensor_3);
+    // Controller controller3(motor_3, analog_sensor_3);
+    rt_printf("controllers are set up \n");
+
+    controller1.start_loop();
+    controller2.start_loop();
+    controller5.start_loop();
+    // controller3.start_loop();
+
+    rt_printf("loops have started \n");
+
+    while(true)
+    {
+        Timer<>::sleep_ms(10);
+    }
+
+    return 0;
+}
---
+++ include/blmc_drivers/devices/leg.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <map>
+
+
+#include <utils/threadsafe_object.hpp>
+#include <utils/threadsafe_timeseries.hpp>
+
+#include <devices/motor.hpp>
+#include <devices/device_interface.hpp>
+
+
+
+class FingerInterface: public DeviceInterface
+{
+public:
+    typedef ThreadsafeTimeseries<double> ScalarTimeseries;
+    template<typename Type> using Ptr = std::shared_ptr<Type>;
+
+    enum MotorMeasurementIndexing {current, position, velocity, encoder_index,
+                                   motor_measurement_count};
+    enum MotorIndexing {interior, center, tip, motor_count};
+
+    /// getters ================================================================
+    // device outputs ----------------------------------------------------------
+    virtual Ptr<const ScalarTimeseries>
+    motor_measurement(const int& motor_index,
+                      const int& measurement_index) const = 0;
+
+    // input logs --------------------------------------------------------------
+    virtual Ptr<const ScalarTimeseries>
+    current_target(const int& motor_index) const = 0;
+    virtual Ptr<const ScalarTimeseries>
+    sent_current_target(const int& motor_index) const = 0;
+
+    /// setters ================================================================
+    virtual void set_current_target(const double& current_target,
+                                    const int& motor_index) = 0;
+
+    /// sender =================================================================
+    virtual void send_if_input_changed() = 0;
+
+    /// ========================================================================
+
+    virtual ~FingerInterface() {}
+};
+
+
+class Finger: public FingerInterface
+{
+private:
+    std::array<std::shared_ptr<MotorInterface>, 3> motors_;
+
+public:
+    Finger(std::shared_ptr<MotorInterface> interior_motor,
+           std::shared_ptr<MotorInterface> center_motor,
+           std::shared_ptr<MotorInterface> tip_motor)
+    {
+        motors_[interior] = interior_motor;
+        motors_[center] = center_motor;
+        motors_[tip] = tip_motor;
+    }
+
+    virtual ~Finger() {}
+
+    /// getters ================================================================
+    // device outputs ----------------------------------------------------------
+    virtual Ptr<const ScalarTimeseries>
+    motor_measurement(const int& motor_index,
+                      const int& measurement_index) const
+    {
+        return motors_[motor_index]->measurement(measurement_index);
+    }
+
+    // input logs --------------------------------------------------------------
+    virtual Ptr<const ScalarTimeseries>
+    current_target(const int& motor_index) const
+    {
+        return motors_[motor_index]->current_target();
+    }
+    virtual Ptr<const ScalarTimeseries>
+    sent_current_target(const int& motor_index) const
+    {
+        return motors_[motor_index]->sent_current_target();
+    }
+
+    /// setters ================================================================
+    virtual void set_current_target(const double& current_target,
+                                    const int& motor_index)
+    {
+        motors_[motor_index]->set_current_target(current_target);
+    }
+
+    /// sender =================================================================
+    virtual void send_if_input_changed()
+    {
+        for(size_t i = 0; i < motors_.size(); i++)
+            motors_[i]->send_if_input_changed();
+    }
+
+    /// ========================================================================
+};
+
