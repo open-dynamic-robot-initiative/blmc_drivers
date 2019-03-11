08a9e5d2a2d171788e8812b132dcb09d334822de
Maximilien Naveau <maximilien.naveau@gmail.com>
2019-03-11 18:37:24 +0100

    add a small demo that control the current in a sinus and that dump the data in a csv format (spaces)

---
+++ demos/sine_torque_control.cpp
@@ -0,0 +1,108 @@
+/**
+ * @file pd_control.cpp
+ * @author Maximilien Naveau (maximilien.naveau@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2018-11-29
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+
+#include <fstream>
+#include "real_time_tools/timer.hpp"
+#include "real_time_tools/spinner.hpp"
+#include "sine_torque_control.hpp"
+
+namespace blmc_drivers{
+
+void SineTorqueControl::loop()
+{
+  const int & blmc_position_index = MotorInterface::MeasurementIndex::position;
+  const int & blmc_velocity_index = MotorInterface::MeasurementIndex::velocity;
+  // some data
+  double actual_position = 0.0;
+  double actual_velocity = 0.0;
+  double local_time = 0.0;
+  // sine torque params
+  double current_amplitude = 0.3;
+  double current_period = 2.0;
+  double current_pulsation = 2*3.1415 / current_period;
+  // here is the control in current (Amper)
+  double desired_current = 0.0;
+
+  real_time_tools::Spinner spinner;
+  spinner.set_period(0.001); // here we spin every 1ms
+  real_time_tools::Timer time_logger;
+  size_t count = 0;
+  while(!stop_loop_)
+  {
+    time_logger.tic();
+    local_time = count * 0.001;
+
+    // compute the control
+    for(int i=0; i<motor_list_.size() ; ++i)
+    {
+      actual_position = motor_list_[i]->get_measurement(
+        blmc_position_index)->newest_element();
+      actual_velocity = motor_list_[i]->get_measurement(
+        blmc_velocity_index)->newest_element();
+      
+      desired_current = current_amplitude * sin(current_pulsation * local_time);
+
+      motor_list_[i]->set_current_target(desired_current);
+      motor_list_[i]->send_if_input_changed();
+
+      encoders_[i].push_back(actual_position);
+      velocities_[i].push_back(actual_velocity);
+      control_buffer_[i].push_back(desired_current);
+
+      // we sleep here 1ms.
+      spinner.spin();
+      // measure the time spent.
+      time_logger.tac();
+
+      // Printings
+      if ((count % 1000) == 0)
+      {
+          rt_printf("sending current: %f\n", desired_current);
+          //time_logger.print_statistics();
+      }
+      ++count;
+    }//endfor
+  }//endwhile
+  time_logger.dump_measurements("/tmp/demo_pd_control_time_measurement");
+}
+
+void SineTorqueControl::stop_loop()
+{
+  // dumping stuff
+  std::string file_name="/tmp/sine_torque_xp.dat";
+  try{
+    std::ofstream log_file(file_name, std::ios::binary | std::ios::out);
+    log_file.precision(10);
+    
+    assert(encoders_[0].size() == velocities_[0].size() &&
+           velocities_[0].size() == control_buffer_[0].size());
+    for(int j=0 ; j<encoders_[0].size() ; ++j)
+    {
+      for(int i=0 ; i<encoders_.size() ; ++i)
+      {  
+        log_file << encoders_[i][j] << " "
+                 << velocities_[i][j] << " "
+                 << control_buffer_[i][j] << " ";
+      }
+      log_file << std::endl;
+    }
+
+    log_file.flush();
+    log_file.close();
+  }catch(...){
+    rt_printf("fstream Error in dump_tic_tac_measurements(): "
+              "no time measurment saved\n");
+  }
+
+  rt_printf("dumped the trajectory");
+}
+
+} // namespace blmc_drivers
---
+++ demos/sine_torque_control.hpp
@@ -0,0 +1,131 @@
+/**
+ * @file pd_control.hpp
+ * @author Maximilien Naveau (maximilien.naveau@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2018-11-29
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
+
+#include "blmc_drivers/devices/motor.hpp"
+#include "blmc_drivers/devices/analog_sensor.hpp"
+
+namespace blmc_drivers{
+
+/**
+ * @brief This is a simple shortcut
+ */
+typedef std::shared_ptr<blmc_drivers::SafeMotor> SafeMotor_ptr;
+
+/**
+ * @brief This is a basic PD controller to be used in the demos of this package.
+ */
+class SineTorqueControl
+{
+public:
+  /**
+   * @brief Construct a new SineTorqueControl object.
+   * 
+   * @param motor_slider_pairs 
+   */
+  SineTorqueControl(std::vector<SafeMotor_ptr> motor_list):
+    motor_list_(motor_list)
+  {
+    encoders_.clear();
+    velocities_.clear();
+    control_buffer_.clear();
+
+    for(int i=0 ; i<motor_list.size() ; ++i)
+    {
+      encoders_.push_back(std::deque<double>());
+      velocities_.push_back(std::deque<double>());
+      control_buffer_.push_back(std::deque<double>());
+      encoders_.back().clear();
+      velocities_.back().clear();
+      control_buffer_.back().clear();
+    }
+    stop_loop_=false;
+  }
+
+  /**
+   * @brief Destroy the SineTorqueControl object
+   */
+  ~SineTorqueControl()
+  {
+    stop_loop_=true;
+    real_time_tools::join_thread(rt_thread_);
+  }
+
+  /**
+   * @brief This method is a helper to start the thread loop.
+   */
+  void start_loop()
+  {
+    real_time_tools::create_realtime_thread(
+          rt_thread_, &SineTorqueControl::loop, this);
+  }
+
+  /**
+   * @brief Stop the control and dump the data
+   */
+  void stop_loop();
+
+private:
+
+  /**
+   * @brief This is list of motors
+   */
+  std::vector<SafeMotor_ptr> motor_list_;
+  /**
+   * @brief This is the real time thread object.
+   */
+  real_time_tools::RealTimeThread rt_thread_;
+
+  /**
+     * @brief this function is just a wrapper around the actual loop function,
+     * such that it can be spawned as a posix thread.
+     */
+  static THREAD_FUNCTION_RETURN_TYPE loop(void* instance_pointer)
+  {
+    ((SineTorqueControl*)(instance_pointer))->loop();
+  }
+
+  /**
+     * @brief this is a simple control loop which runs at a kilohertz.
+     *
+     * it reads the measurement from the analog sensor, in this case the
+     * slider. then it scales it and sends it as the current target to
+     * the motor.
+     */
+  void loop();
+
+  /**
+   * @brief managing the stopping of the loop
+   */
+  bool stop_loop_;
+
+  /**
+   * @brief memory_buffer_size_ is the max size of the memory buffer.
+   */
+  unsigned memory_buffer_size_;
+
+  /**
+   * @brief Encoder data
+   */
+  std::vector<std::deque<double> > encoders_;
+  
+  /**
+   * @brief Velcoity data
+   */
+  std::vector<std::deque<double> > velocities_;
+
+  /**
+   * @brief control_buffer_
+   */
+  std::vector<std::deque<double> > control_buffer_;
+
+}; // end class SineTorqueControl definition
+
+}// namespace blmc_drivers
