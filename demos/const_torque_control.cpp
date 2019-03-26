d9b456dd4c9d60f9ddbfe4e21842ed06ef0594d5
Steve Heim <heim@is.mpg.de>
2019-03-26 08:56:53 +0100

    const torque for testing dual encoders

--- demos/const_torque_control.cpp
+++ demos/const_torque_control.cpp
@@ -12,11 +12,11 @@
 #include <fstream>
 #include "real_time_tools/timer.hpp"
 #include "real_time_tools/spinner.hpp"
-#include "sine_torque_control.hpp"
+#include "const_torque_control.hpp"
 
 namespace blmc_drivers{
 
-void SineTorqueControl::loop()
+void ConstTorqueControl::loop()
 {
   const int & blmc_position_index = MotorInterface::MeasurementIndex::position;
   const int & blmc_velocity_index = MotorInterface::MeasurementIndex::velocity;
@@ -26,10 +26,6 @@ void SineTorqueControl::loop()
   double actual_velocity = 0.0;
   double actual_current = 0.0;
   double local_time = 0.0;
-  // sine torque params
-  double current_amplitude = 0.0;
-  double current_period = 2.0;
-  double current_pulsation = 2*3.1415 / current_period;
   // here is the control in current (Ampere)
   double desired_current = 0.0;
 
@@ -52,8 +48,7 @@ void SineTorqueControl::loop()
       actual_current = motor_list_[i]->get_measurement(
         blmc_current_index)->newest_element();
       
-      desired_current = current_amplitude * sin(current_pulsation * local_time);
-
+      // desired_current = 0.1;
       motor_list_[i]->set_current_target(desired_current);
       motor_list_[i]->send_if_input_changed();
 
@@ -79,10 +74,10 @@ void SineTorqueControl::loop()
   time_logger.dump_measurements("/tmp/demo_pd_control_time_measurement");
 }
 
-void SineTorqueControl::stop_loop()
+void ConstTorqueControl::stop_loop()
 {
   // dumping stuff
-  std::string file_name="/tmp/sine_torque_xp.dat";
+  std::string file_name="/tmp/Const_torque_xp.dat";
   try{
     std::ofstream log_file(file_name, std::ios::binary | std::ios::out);
     log_file.precision(10);
